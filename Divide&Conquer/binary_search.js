f856057029ad130fdf6bc580056ec807864d6836
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-11-14 18:01:17 -0800

    Pushing binary search

--- Divide&Conquer/binary_search.js
+++ Divide&Conquer/binary_search.js
@@ -1,5 +1,25 @@
 /*
  * Binary Search
  * Description:
+ * Given a sorted (in ascending order) integer array nums of n elements and a
+ * target value, write a function to search target in nums. If target exists,
+ * then return its index, otherwise return -1.
+ * 
+ * You may assume that all elements in nums are unique.
+ * n will be in the range [1, 10000].
+ * The value of each element in nums will be in the range [-9999, 9999].
+ *
+ * Test Case: 
+ * nums = [-1,0,3,5,9,12]
+ * target = 9
  * 
 */
+
+var search = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+};
